47e5f7181890ef22b6ce3c85cb2472fd84807eb1
Suraj Sharma <surajkoho@gmail.com>
2017-11-08 19:10:11 +0530

    User Signs up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
-import auth from './routes/auth';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
+
+import auth from './routes/auth';
+import users from './routes/users';
 
 dotenv.config();
 
@@ -13,7 +15,9 @@ app.use(bodyParser.json());
 
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL,{useMongoClient:true});
+
 app.use('/api/auth',auth);
+app.use('/api/users', users);
 
 app.get('/*',(req,res)=>{
 	res.sendFile(path.join(__dirname,'index.html'));
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 //TODO: add uniqueness & email validations to email field 
 
@@ -10,11 +11,16 @@ const schema = new mongoose.Schema(
 			type:String, 
 			required: true,
 			lowercase: true,
-			index: true
+			index: true,
+			unique:true
 		},
 		passwordHash: {
 			type: String,
 		    required:true
+		},
+		confirmed: {
+			type: Boolean,
+			default: false
 		}
 	},
 
@@ -25,6 +31,10 @@ const schema = new mongoose.Schema(
 
 schema.methods.isValidPassword = function isValidPassword(password){
 	return bcrypt.compareSync(password,this.passwordHash);
+};
+
+schema.methods.setPassword = function setPassword(password){
+	this.passwordHash = bcrypt.hashSync(password,10);
 };
 
 schema.methods.generateJWT = function generateJWT(){
@@ -39,8 +49,12 @@ schema.methods.generateJWT = function generateJWT(){
 schema.methods.toAuthJSON = function toAuthJSON(){
 	return {
 		email: this.email,
+		confirmed: this.confirmed,
 		token: this.generateJWT()
 	}
 };
+
+
+schema.plugin(uniqueValidator,{message:"This email is already taken"});
 
 export default mongoose.model("User",schema);
